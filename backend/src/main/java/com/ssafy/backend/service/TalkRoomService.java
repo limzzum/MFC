38e36707e6835b6c39e5a9a7eb12fd1133368a73
jiwon <wldnjs9515@naver.com>
2023-07-31 23:36:08 +0900

    feat : talkroomService 작성

---
+++ backend/src/main/java/com/ssafy/backend/service/TalkRoomService.java
@@ -0,0 +1,12 @@
+package com.ssafy.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class TalkRoomService {
+}
