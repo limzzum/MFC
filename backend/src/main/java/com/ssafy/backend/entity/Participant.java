b9bdfd58e4c094ec5b768e4681096fa96fc87229
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-07-31 16:01:29 +0900

    feat : Participant엔티티 작성

---
+++ backend/src/main/java/com/ssafy/backend/entity/Participant.java
@@ -0,0 +1,62 @@
+package com.ssafy.backend.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.Name;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Table(name = "participant")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Participant {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "participant_id", updatable = false)
+  private Long id;
+
+  @Column(name = "nick_name", nullable = false, length = 24)
+  private String nickName;
+
+  @Column(name = "is_vote_type_a")
+  private Boolean isVoteTypeA;
+
+  @Column(name = "is_host", nullable = false, columnDefinition = "BOOLEAN default false" )
+  private boolean isHost;
+
+  @Column(name = "vote_time")
+  private LocalDateTime voteTime;
+
+  @Column(name = "enter_time", nullable = false)
+  private LocalDateTime enterTime;
+
+  @OneToMany(mappedBy = "Participant")
+  private List<User> users = new ArrayList<>();
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "talkroom_id")
+  private TalkRoom talkroom;
+
+  @OneToOne
+  @JoinColumn(name = "role_code_id")
+  private RoleCode roleCode;
+
+}
