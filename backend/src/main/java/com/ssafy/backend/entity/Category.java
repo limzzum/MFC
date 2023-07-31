527ab3d62c8eef43a8fd384bc4ed61b69dcd371f
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-07-31 16:01:06 +0900

    feat : Category엔티티 작성

---
+++ backend/src/main/java/com/ssafy/backend/entity/Category.java
@@ -0,0 +1,30 @@
+package com.ssafy.backend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Table(name = "category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "category_id", updatable = false)
+  private Long id;
+
+  @Column(nullable = false, length = 10)
+  private int name;
+
+}
