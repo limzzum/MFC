76af035dd2f5778384667a990a994070b8faa0fd
suchshin <scshin97@gmail.com>
2023-08-01 15:04:35 +0900

    feat : 메인페이지와 토론방 카드 UI 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import LoginPage from './pages/login/loginPage';
 import PasswordChangePage from './pages/passwordchange/passwordChangePage';
 import SignupPage from './pages/signup/signupPage';
+import MainPage from './pages/main/mainPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
         <nav>
-          <ul className='horizontal-list'>
+          <ul className='horizontal-align'>
             <li>
               <Link to='/pages/login/loginPage'>loginPage</Link>
             </li>
@@ -20,18 +21,19 @@ function App() {
             <li>
               <Link to='/pages/signup/signupPage'>signupPage</Link>
             </li>
+            <li>
+              <Link to='/pages/main/mainPage'>mainPage</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
-          <Route path='/pages/login/loginPage' element={<LoginPage/>}>
-          </Route>
-          <Route path='/pages/passwordchange/passwordChangePage' element={<PasswordChangePage/>}>
-          </Route>
-          <Route path='/pages/signup/signupPage' element={<SignupPage/>}>
-          </Route>
+          <Route path='/pages/login/loginPage' element={<LoginPage />} />
+          <Route path='/pages/passwordchange/passwordChangePage' element={<PasswordChangePage />} />
+          <Route path='/pages/signup/signupPage' element={<SignupPage />} />
+          <Route path='/pages/main/mainPage' element={<MainPage />} />
         </Routes>
-      </div>  
-    </BrowserRouter> 
+      </div>
+    </BrowserRouter>
   );
 }
 
