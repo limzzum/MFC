c1636efb29b9871063201f776c68b1fc63e06638
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-08-01 12:03:26 +0900

    feat : TalkRoomController 수정

--- backend/src/main/java/com/ssafy/backend/controller/TalkRoomController.java
+++ backend/src/main/java/com/ssafy/backend/controller/TalkRoomController.java
@@ -5,6 +5,7 @@ import com.ssafy.backend.service.TalkRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +14,28 @@ import java.awt.print.Pageable;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/debate")
+g@RequestMapping("/debate")
 public class TalkRoomController {
 
     private final TalkRoomService talkRoomService;
 
-    @GetMapping("/listPerPage")
-    public ResponseEntity<?> talkroomlist(Pageable pageable) {
+    @GetMapping("/listongoing")
+    public ResponseEntity<?> talkroomlist(@RequestParam Long minTalkRoomId,@RequestParam int size) {
+        try {
+            List<TalkRoomListDto> talkRoomList = talkRoomService.
+        }
+
+    }
+
+    @GetMapping("/listwaiting")
+    public ResponseEntity<?> talkroomlist(@RequestParam Long minTalkRoomId,@RequestParam int size) {
         try {
             List<TalkRoomListDto> talkRoomListDtos
         }
 
     }
+
+    @PostMapping
+    public ResponseEntity<?> talkroomcreate()
 
 }
