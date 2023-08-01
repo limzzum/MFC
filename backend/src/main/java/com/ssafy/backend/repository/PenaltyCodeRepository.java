3210d7dcdc991d6c2944f8cfc8a44af83eea8da4
kosigi19 <kosigi19@naver.com>
2023-08-01 17:59:01 +0900

    feat : 페널티 등록 기능 API 추가

---
+++ backend/src/main/java/com/ssafy/backend/repository/PenaltyCodeRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.backend.repository;
+
+import com.ssafy.backend.entity.PenaltyCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PenaltyCodeRepository extends JpaRepository<PenaltyCode, Long> {
+
+}
