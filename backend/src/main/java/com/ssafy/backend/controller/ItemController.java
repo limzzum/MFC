1c9b02432a7a777c0f944f87f2d47ad5e3bcce0e
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-08-01 17:49:40 +0900

    feat : ItemController 수정

--- backend/src/main/java/com/ssafy/backend/controller/ItemController.java
+++ backend/src/main/java/com/ssafy/backend/controller/ItemController.java
@@ -1,10 +1,10 @@
 package com.ssafy.backend.controller;
 
 import com.ssafy.backend.service.ItemService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/item")
@@ -14,6 +14,7 @@ public class ItemController {
   @GetMapping("/list")
   public ResponseEntity<?> itemcodelist() {
     //try { 회원하면 예외처리하기
+    System.out.println("____________________________________");
     return ResponseEntity.ok(itemService.findAll());
     //} catch ()
   }
