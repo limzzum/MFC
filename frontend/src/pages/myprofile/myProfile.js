27c56e6db302cdc56c25be5b2b16f19da4666a3a
김의준 <euijun98@naver.com>
2023-08-01 16:56:39 +0900

    feat : 마이페이지 이미지 수정 기능 추가

--- frontend/src/pages/myprofile/myProfile.js
+++ frontend/src/pages/myprofile/myProfile.js
@@ -1,49 +1,87 @@
-// import { useState, useEffect } from "react";
-import styles from './myProfile.module.css'
-import profileImage from '../../images/img.jpg'
+import { useState } from "react";
+import styles from './myProfile.module.css';
+import profileImage from '../../images/img.jpg';
+import settingIcon from '../../images/settingIcon.png';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Row } from "react-bootstrap";
 
 function MyProfile() {
+  const [selectedImage, setSelectedImage] = useState(null);
 
-    return (
-      <div className={styles.wrapper}>
-        <p className={styles.profileTitle}>My Profile</p>
-        <div>
-          <hr />
-            <form>
-            <div className={styles.profileImage}>
-                <img className={`${styles.radiusImg}`} src={profileImage} alt="profileImage" />
-                <a href="https://www.naver.com"><img className={`${styles.radiusImg} ${styles.imgSetting}`} src={profileImage} alt="profileImage" />
-                </a>
-            </div>
-            <div className={styles.profileText}>
-                <ul>
-                <li>
-                  <label for="이메일" className="mb-2">이메일</label>
-                  <input className="form-control w-75" type="text" placeholder="email"></input>
-                </li>
-                <li>
-                  <label for="Nickname" className="mb-2">닉네임 변경</label>
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    // 선택된 파일(file)을 상태(selectedImage)에 저장
+    setSelectedImage(file);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.profileTitle}>My Profile</p>
+      <div>
+        <hr />
+        <form>
+          <div className={styles.profileImage}>
+            {selectedImage ? (
+              <img
+                className={`${styles.radiusImg}`}
+                src={URL.createObjectURL(selectedImage)}
+                alt="profileImage"
+              />
+            ) : (
+              <img className={`${styles.radiusImg}`} src={profileImage} alt="profileImage" />
+            )}
+            <label htmlFor="fileInput" className={`${styles.radiusImg} ${styles.imgSetting}`}>
+              <img src={settingIcon} alt="이미지변경" />
+              <input
+                id="fileInput"
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                style={{ display: "none" }}
+              />
+            </label>
+          </div>
+          <div className={styles.profileText}>
+            <ul>
+              <li>
+                <label htmlFor="이메일" className="mb-2">
+                  이메일
+                </label>
+                <input className="form-control w-75" type="text" placeholder="email"></input>
+              </li>
+              <li>
+                <label htmlFor="Nickname" className="mb-2">
+                  닉네임 변경
+                </label>
                 <div className="input-group mb-4 w-75">
-                  <input type="text" class="form-control" placeholder="닉네임 적어줘" aria-label="Nickname" aria-describedby="basic-addon2"/>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="닉네임 적어줘"
+                    aria-label="Nickname"
+                    aria-describedby="basic-addon2"
+                  />
                   <div className="input-group-append">
-                    <button className="btn btn-outline-secondary" type="button">중복확인</button>
+                    <button className="btn btn-outline-secondary" type="button">
+                      중복확인
+                    </button>
                   </div>
                 </div>
-                </li>
-                </ul>
-            </div>
-            </form>
+              </li>
+            </ul>
           </div>
-          <div>
-          <Row>
-              <Button className="col-4 btn btn-primary w-150px m-auto" type="submit">변경</Button>
-              <Button className="col-4 btn btn-danger w-150px m-auto">탈퇴</Button>
-          </Row>
-        </div>
+        </form>
       </div>
-    );
+      <div>
+        <Row>
+          <Button className="col-4 btn btn-primary w-150px m-auto" type="submit">
+            변경
+          </Button>
+          <Button className="col-4 btn btn-danger w-150px m-auto">탈퇴</Button>
+        </Row>
+      </div>
+    </div>
+  );
 }
 
 export default MyProfile;
