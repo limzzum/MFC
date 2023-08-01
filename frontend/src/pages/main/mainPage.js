3cda8c63c1aae32c815f1ba3141af0159dd1ad9e
suchshin <scshin97@gmail.com>
2023-08-01 17:08:57 +0900

    feat : 메인페이지에 있는 토론방 UI 컴포넌트화

--- frontend/src/pages/main/mainPage.js
+++ frontend/src/pages/main/mainPage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import style from './mainPage.module.css';
-import personImage from '../../images/person.png'
 import { BsPlusSquare } from "react-icons/bs";
-import { BsStopwatch } from "react-icons/bs";
-import { RiSpeakLine } from "react-icons/ri";
+import DebateRoomCard from '../../components/mainpage/debateRoomCard';
 
 function mainPage() {
   return (
@@ -21,42 +19,10 @@ function mainPage() {
 
         <hr className={style.horizontalline} />
 
-        <div className=''>
-          <div className="card" style={{ width: '18rem' }}>
-            <div className="d-flex">
-              <div className={style.imgbox}>
-                <img src={personImage} style={{ maxWidth: '100%' }} alt="..." />
-              </div>
-              <div className={style.imgbox}>
-                <img src={personImage} style={{ maxWidth: '100%' }} alt="..." />
-              </div>
-            </div>
-
-
-            <div className={style.cardbody}>
-              <p className={style.cardtitle}>송강 호떡 사주기 VS 송강호 떡 사주기</p>
-
-              <div>
-                <BsStopwatch className={style.timeicon} />
-                <span className="card-text">토론 시간</span>
-              </div>
-              <div>
-                <RiSpeakLine className={style.timeicon} />
-                <span className="card-text">발언 제한 시간</span>
-              </div>
-
-            </div>
-
-
-            <div className="card-body">
-              <div className={style.joinbuttoncontainer}>
-                <button className={style.joinbutton}>
-                  참여하기
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+       
+         <div>
+            <DebateRoomCard/>
+         </div>
 
         <hr className={style.horizontalline} />
 
