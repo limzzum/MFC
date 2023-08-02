b7d78814ab7e0334510dabdec56c17d89b6557df
kosigi19 <kosigi19@naver.com>
2023-08-02 10:37:37 +0900

    feat : Repository 파일 추가

---
+++ backend/src/main/java/com/ssafy/backend/repository/RoleCodeRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.backend.repository;
+
+import com.ssafy.backend.entity.RoleCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleCodeRepository extends JpaRepository<RoleCode, Long> {
+
+}
