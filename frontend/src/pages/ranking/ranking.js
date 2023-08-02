4ed97cd34c49817aac840a3a98d4227ca273deb4
김의준 <euijun98@naver.com>
2023-08-02 16:53:07 +0900

    feat : 랭킹페이지 UI 변경

--- frontend/src/pages/ranking/ranking.js
+++ frontend/src/pages/ranking/ranking.js
@@ -1,44 +1,29 @@
 import RankingProfile from '../../components/rankingpage/rankingProfile.js'
 import RankMyProfile from '../../components/rankingpage/rankMyProfile.js'
 import RankingSearchBar from '../../components/rankingpage/rankingSearchBar.js'
+import style from './ranking.module.css';
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from 'react-bootstrap';
 
 function Ranking() {
+
+    
     return (
-        <div className="wrapper mx-auto">
-            <Container>
-                <Row> 
-                    <Col>
-                        <RankMyProfile />
-                    </Col>
-                    <Col>
-                        <RankingSearchBar />
-                        <Container>
-                            <Row>
-                                <Col>
-                                    랭킹
-                                </Col>
-                                <Col>
-                                </Col>
-                                <Col>
-                                    유저명
-                                </Col>
-                                <Col>
-                                    경험치
-                                </Col>
-                                <Col>
-                                    승률
-                                </Col>
-                            </Row>
-                        </Container>
-                        <div className="mx-auto">
-                            <RankingProfile />
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+        <div className={style.wrapper}>
+            <RankMyProfile />
+            <div className={style.nextBox}> 
+            <RankingSearchBar />
+            <div className={style.rankTitleBox}>
+                <div className={style.rankTitle}>랭킹</div>
+                <div className={style.rankTitle}></div>
+                <div className={style.rankTitle}>유저명</div>
+                <div className={style.rankTitle}>경험치</div>
+                <div className={style.rankTitle}>승률</div>
+            </div>
+            <div className="mx-auto">
+                <RankingProfile />
+            </div>
+        </div>
         </div>
     );
 
