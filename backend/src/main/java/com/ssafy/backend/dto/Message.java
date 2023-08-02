36fc0e288d3bbc279d83ee7f15ba445f6c451355
kosigi19 <kosigi19@naver.com>
2023-08-02 15:20:39 +0900

    feat : ResponseBody Message수정

--- backend/src/main/java/com/ssafy/backend/dto/Message.java
+++ backend/src/main/java/com/ssafy/backend/dto/Message.java
@@ -2,46 +2,31 @@ package com.ssafy.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 /*
  * Message message = new Message();
- * message.setStatus(StatusEnum.CREATED);
- * message.setMessage("성공 코드");
+ * message.setStatus(HttpStatus.OK);
+ * message.setMessage("message");
  * message.setData(reqBody);
+ * return ResponseEntity.ok(message);
+ *
+ * Message message = new Message(HttpStatus.OK, "message", reqBody);
  * return ResponseEntity.ok(message);
  * */
 @Data
 @AllArgsConstructor
 public class Message {
 
-  private StatusEnum status;
+  private HttpStatus status;
   private String message;
   private Object data;
 
   public Message() {
-    this.status = StatusEnum.BAD_REQUEST;
+    this.status = HttpStatus.BAD_REQUEST;
     this.data = null;
     this.message = null;
   }
-
-  public enum StatusEnum {
-
-    OK(200, "OK"),
-    CREATED(201, "CREATED"),
-    BAD_REQUEST(400, "BAD_REQUEST"),
-    NOT_FOUND(404, "NOT_FOUND"),
-    INTERNAL_SERER_ERROR(500, "INTERNAL_SERVER_ERROR");
-
-    int statusCode;
-    String code;
-
-    StatusEnum(int statusCode, String code) {
-      this.statusCode = statusCode;
-      this.code = code;
-    }
-  }
-
-
 }
 
 
