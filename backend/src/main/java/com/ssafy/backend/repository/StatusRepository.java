c74efb4b40b67c7cd8fcbf698086a824d6c72ece
kosigi19 <kosigi19@naver.com>
2023-08-02 15:43:11 +0900

    feat : feature-BE-PenaltyAPI수정

--- backend/src/main/java/com/ssafy/backend/repository/StatusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.backend.repository;
-
-import com.ssafy.backend.entity.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Long> {
-
-}
