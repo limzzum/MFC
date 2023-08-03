4fc379b9e2512d5c23d952cc6581469282ab31d3
kosigi19 <kosigi19@naver.com>
2023-08-03 17:25:47 +0900

    feat : 관전자입장 api 개발완료

--- backend/src/main/java/com/ssafy/backend/repository/ParticipantRepository.java
+++ backend/src/main/java/com/ssafy/backend/repository/ParticipantRepository.java
@@ -1,11 +1,15 @@
 package com.ssafy.backend.repository;
 
 import com.ssafy.backend.entity.Participant;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-    List<Participant> findAllByRoomId(Long roomId);
-    List<Participant> findAllByRoleCodeId(Long roleCodeId);
+
+  List<Participant> findAllByRoomId(Long roomId);
+
+  List<Participant> findAllByRoleCodeId(Long roleCodeId);
+
+  Participant findAllByUserIdAndRoomId(Long userId, Long roomId);
+
 }
