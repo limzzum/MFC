d4fe534a36b1afcbb5bf809076ffa290021cc021
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-08-03 22:30:44 +0900

    feat : 토론방 리셋 API 관련 메서드 추가

--- backend/src/main/java/com/ssafy/backend/service/ParticipantService.java
+++ backend/src/main/java/com/ssafy/backend/service/ParticipantService.java
@@ -18,20 +18,24 @@ public class ParticipantService {
   private final ParticipantRepository participantRepository;
   private final PlayerRepository playerRepository;
 
-//  public void resetParticipants(Long roomId) {
-//    List<Participant> participants = participantRepository.findAllByRoomId(roomId);
-//
-//    if(!participants.isEmpty()) {
-//      for(Participant p : participants) {
-//        if(p.getRoleCode().getId() == 2) {
-//          Player player = playerRepository.findAll
-//        }
-//
-//
-//        p.setIsVoteTypeA(null);
-//        p.setVoteTime(null);
-//      }
-//      participantRepository.saveAll(participants);
-//    }
-//  }
+  public void resetParticipants(Long roomId) {
+    List<Participant> participants = participantRepository.findAllByRoomId(roomId);
+
+    if(!participants.isEmpty()) {
+      for(Participant p : participants) {
+        if(p.getRoleCode().getId() == 2) {
+          Player player = playerRepository.findTopByRoomIdAndUserId(roomId,p.getUser().getId()).orElse(null);
+          if(player != null) {
+            player.setReady(false);
+            player.setHeartPoint(100);
+            player.setRemainOverTimeCount(p.getRoom().getOverTimeCount());
+            playerRepository.save(player);
+          }
+        }
+        p.setIsVoteTypeA(null);
+        p.setVoteTime(null);
+      }
+      participantRepository.saveAll(participants);
+    }
+  }
 }
