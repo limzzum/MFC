7d0874fc3315c6e738ef680270d00226a03c7948
limzzum <chfhddl1234@naver.com>
2023-08-04 03:25:57 +0900

    refactor : User 반환값 Optional로 변경

--- backend/src/main/java/com/ssafy/backend/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/backend/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.ssafy.backend.entity.User;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
     User findByNickname(String nickname);
 }
