01aace79dc479807b717e06f7b4133fb3f5a4120
limzzum <chfhddl1234@naver.com>
2023-08-04 03:27:30 +0900

    feat : email 중복 검사 service 추가

--- backend/src/main/java/com/ssafy/backend/service/UserService.java
+++ backend/src/main/java/com/ssafy/backend/service/UserService.java
@@ -9,6 +9,7 @@ import com.ssafy.backend.entity.User;
 import com.ssafy.backend.repository.HistoryRepository;
 import com.ssafy.backend.repository.UserRepository;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -17,77 +18,89 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-  private final UserRepository repository;
-  private final HistoryRepository historyRepository;
+    private final UserRepository repository;
+    private final HistoryRepository historyRepository;
 
-  public Long regist(UserRegistDto user) {
-    User registUser = User.builder().email(user.getEmail()).nickname(user.getNickname())
-        .password(user.getPassword())
-        .profile(user.getProfile()).build();
-    User saved = repository.save(registUser);
-    return saved.getId();
-  }
-
-  public Long login(LoginForm loginForm) {
-    User user = repository.findByEmail(loginForm.getEmail());
-    if (user == null) {
-      return null;
+    public Long regist(UserRegistDto user) {
+        User registUser = User.builder().email(user.getEmail()).nickname(user.getNickname())
+                .password(user.getPassword())
+                .profile(user.getProfile()).build();
+        User saved = repository.save(registUser);
+        return saved.getId();
     }
-    if (user.getPassword().equals(loginForm.getPassword())) {
-      return user.getId();
-    }
-    return null;
-  }
 
-  public void logout(Long userId) {
-
-  }
-
-  public User findUser(Long id) {
-    return repository.findById(id).orElseGet(null);
-  }
-
-  public User findById(Long id) {
-    if(repository.findById(id).isEmpty())
-      return null;
-    else
-      return repository.findById(id).get();
-  }
-
-  public Message userItemCodeUpdate(Long userId, ItemCode itemCode) {
-    Message message = new Message();
-    Optional<User> userOP = repository.findById(userId);
-    if(userOP.isPresent()) {
-      User user = userOP.get();
-      if(itemCode.equals(user.getColorItem())) {
-        message.setData(HttpStatus.BAD_REQUEST);
-        message.setMessage("유저가 이미 해당 아이템을 보유하고 있습니다.");
-      } else {
-        History history = historyRepository.findTopByUserId(userId);
-        if(history == null) {
-          message.setStatus(HttpStatus.BAD_REQUEST);
-          message.setMessage("해당 유저의 전적을 찾을 수 없습니다.");
-        }else {
-          int userCoin = history.getCoin() - itemCode.getPrice();
-          if(userCoin < 0) {
-            message.setStatus(HttpStatus.BAD_REQUEST);
-            message.setMessage("유저의 코인이 부족해 아이템을 구매할 수 없습니다.");
-          }else {
-            history.setCoin(userCoin);
-            historyRepository.save(history);
-            user.setColorItem(itemCode);
-            repository.save(user);
-            message.setStatus(HttpStatus.OK);
-            message.setMessage("유저의 닉네임 커스팀이 변경되었습니다.");
-          }
+    public Long login(LoginForm loginForm) {
+        Optional<User> user = repository.findByEmail(loginForm.getEmail());
+        if (!user.isPresent()) {
+            return null;
         }
-      }
-    } else {
-      message.setStatus(HttpStatus.BAD_REQUEST);
-      message.setMessage("해당 유저를 찾을 수 없습니다.");
+
+        if (user.get().getPassword().equals(loginForm.getPassword())) {
+            return user.get().getId();
+        }
+        return null;
     }
-    return message;
-  }
+
+    public void logout(Long userId) {
+
+    }
+
+    public User findUser(Long id) {
+        return repository.findById(id).orElseGet(null);
+    }
+
+    public User findById(Long id) {
+        if (repository.findById(id).isEmpty())
+            return null;
+        else
+            return repository.findById(id).get();
+    }
+
+    public Message userItemCodeUpdate(Long userId, ItemCode itemCode) {
+        Message message = new Message();
+        Optional<User> userOP = repository.findById(userId);
+        if (userOP.isPresent()) {
+            User user = userOP.get();
+            if (itemCode.equals(user.getColorItem())) {
+                message.setData(HttpStatus.BAD_REQUEST);
+                message.setMessage("유저가 이미 해당 아이템을 보유하고 있습니다.");
+            } else {
+                History history = historyRepository.findTopByUserId(userId);
+                if (history == null) {
+                    message.setStatus(HttpStatus.BAD_REQUEST);
+                    message.setMessage("해당 유저의 전적을 찾을 수 없습니다.");
+                } else {
+                    int userCoin = history.getCoin() - itemCode.getPrice();
+                    if (userCoin < 0) {
+                        message.setStatus(HttpStatus.BAD_REQUEST);
+                        message.setMessage("유저의 코인이 부족해 아이템을 구매할 수 없습니다.");
+                    } else {
+                        history.setCoin(userCoin);
+                        historyRepository.save(history);
+                        user.setColorItem(itemCode);
+                        repository.save(user);
+                        message.setStatus(HttpStatus.OK);
+                        message.setMessage("유저의 닉네임 커스팀이 변경되었습니다.");
+                    }
+                }
+            }
+        } else {
+            message.setStatus(HttpStatus.BAD_REQUEST);
+            message.setMessage("해당 유저를 찾을 수 없습니다.");
+        }
+        return message;
+    }
+
+    public boolean isUsedEmail(String email) {
+        Optional<User> byEmail = repository.findByEmail(email);
+        if (byEmail.isPresent()) {
+            return true;
+        }
+        return false;
+
+
+    }
+
 
 //  public User findByNickname(String nickname) {
 //    if(repository.findById(nickname).isEmpty())
