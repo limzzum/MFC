64307405761b968b081ff5c4a8b0746644462e79
limzzum <chfhddl1234@naver.com>
2023-08-04 03:16:23 +0900

    feat : 이메일 인증 토큰 검사 위한 redis추가

--- backend/src/main/java/com/ssafy/backend/util/RedisUtil.java
+++ backend/src/main/java/com/ssafy/backend/util/RedisUtil.java
@@ -14,6 +14,9 @@ public class RedisUtil {
 
     private final RedisTemplate<String, Object> redisTemplate;
     private final RedisTemplate<String, Object> redisBlackListTemplate;
+    private final RedisTemplate<String, Object> emailTokenTemplate;
+    private final RedisTemplate<String, Object> registUserTemplate;
+
 
     @Value("${jwt.expmin}")
     private int expMin;
@@ -51,4 +54,27 @@ public class RedisUtil {
     public boolean hasKeyExcludeList(String key) {
         return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(key));
     }
+
+    public void setEmailTokenTemplate(String key, Object o, int minutes) {
+        emailTokenTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+        emailTokenTemplate.opsForValue().set(key, o, minutes, TimeUnit.MINUTES);
+    }
+
+    public Object getTokenByEmail(String key) {
+        return emailTokenTemplate.opsForValue().get(key);
+    }
+
+    public boolean hasKeyEmail(String key) {
+        return Boolean.TRUE.equals(emailTokenTemplate.hasKey(key));
+    }
+
+    public void setRegistUserTemplate(String key, Object o, int minutes) {
+        registUserTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+        registUserTemplate.opsForValue().set(key, o, minutes, TimeUnit.MINUTES);
+    }
+
+    public Object getRegistUserInfo(String key) {
+        return registUserTemplate.opsForValue().get(key);
+    }
+
 }
