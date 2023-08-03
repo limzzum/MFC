15eff00b505b5428f8d3849a35023df1a8c54a55
kosigi19 <kosigi19@naver.com>
2023-08-03 13:14:03 +0900

    feat : 랭킹API개발완료

--- backend/src/main/java/com/ssafy/backend/controller/HistoryController.java
+++ backend/src/main/java/com/ssafy/backend/controller/HistoryController.java
@@ -3,19 +3,23 @@ package com.ssafy.backend.controller;
 import com.ssafy.backend.dto.Message;
 import com.ssafy.backend.dto.request.HistoryReqDto;
 import com.ssafy.backend.dto.response.HistoryResDto;
+import com.ssafy.backend.dto.response.getHistoryDto;
 import com.ssafy.backend.entity.History;
 import com.ssafy.backend.entity.User;
 import com.ssafy.backend.service.HistoryService;
 import com.ssafy.backend.service.PenaltyCodeService;
-import com.ssafy.backend.service.PenaltyService;
 import com.ssafy.backend.service.UserService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/record")
@@ -31,15 +35,14 @@ public class HistoryController {
     User user = userService.findById(request.getUserId()); // User 조회
     Message message = new Message(HttpStatus.CREATED, "전적 추가 완료", request);
 
-    if(!historyService.existByUserId(request.getUserId())) {//아이디가 없으면
+    if (!historyService.existByUserId(request.getUserId())) {//아이디가 없으면
       History history = History.builder()
-              .user(user)
-              .coin(request.getCoin())
-              .experience(request.getExp())
-              .build();
+          .user(user)
+          .coin(request.getCoin())
+          .experience(request.getExp())
+          .build();
       historyService.save(history);
-    }
-    else {//있으면
+    } else {//있으면
       message.setStatus(HttpStatus.OK);
       message.setMessage("전적 변경 완료");
       historyService.change(request);
@@ -52,39 +55,30 @@ public class HistoryController {
 
     HistoryResDto historyResDto = historyService.findById(userId);
     Message message = new Message(HttpStatus.OK, "전적 조회 성공",
-            historyResDto);
+        historyResDto);
 
-    if(historyResDto == null){
+    if (historyResDto == null) {
       message.setMessage("조회된 전적이 없습니다.");
       return ResponseEntity.ok(message);
     }
 
     return ResponseEntity.ok(message);
-
   }
-//
-//  @GetMapping("/list")
-//  public ResponseEntity<Message> getAllRank(@PageableDefault Pageable pageable){
-//    Message message = new Message(HttpStatus.OK, "랭킹 조회 완료", historyService.getAllHistory(pageable));
-//    return ResponseEntity.ok(message);
-//  }
 
-//  @GetMapping
-//  public ResponseEntity<Message> getPenaltyCode() {
-//    Message message = new Message(HttpStatus.OK, "패널티 코드 조회 완료", penaltyCodeService.findAll());
-//    return ResponseEntity.ok(message);
-//  }
-//
-//  @GetMapping("/list/{roomId}/{userId}")
-//  public ResponseEntity<Message> getPenaltyListById(@PathVariable Long roomId, @PathVariable Long userId) {
-//    List<PenaltyDto> penaltyDtoList = penaltyService.findAllPenalty(userId);
-//    Message message = new Message(HttpStatus.OK, "패널티 조회 성공",
-//            penaltyDtoList);
-//    if(penaltyDtoList.isEmpty()){
-//      message.setMessage("조회된 패널티가 없습니다.");
-//      return ResponseEntity.ok(message);
-//    }
-//    return ResponseEntity.ok(message);
-//  }
+  @GetMapping("/list")
+  public ResponseEntity<?> findAllHistory(@RequestParam int page, @RequestParam int perPage,
+      @RequestParam String keyword) {
 
+    List<HistoryResDto> historyResDtoList = historyService.getRank(page, perPage, keyword);
+
+    getHistoryDto response = new getHistoryDto(
+        historyService.getHistoryTotalPages(page, perPage, keyword),
+        historyResDtoList);
+
+    Message message = new Message(HttpStatus.OK, "랭킹 조회 성공", response);
+    if (historyResDtoList.isEmpty()) {
+      message.setMessage("랭킹 목록이 없습니다.");
+    }
+    return ResponseEntity.ok(message);
+  }
 }
--- backend/src/main/java/com/ssafy/backend/repository/HistoryRepository.java
+++ backend/src/main/java/com/ssafy/backend/repository/HistoryRepository.java
@@ -1,7 +1,6 @@
 package com.ssafy.backend.repository;
 
 import com.ssafy.backend.entity.History;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,8 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
-    @Query("select h from history h join fetch h.user WHERE h.user.id =:userId")
-    History findHistoryByUserId(Long userId);
+  @Query("select h from history h join fetch h.user WHERE h.user.id =:userId")
+  History findHistoryByUserId(Long userId);
 
-//    Page<History> findAllByOrderByRankAtAsc(Pageable pageable);
+  Page<History> findAllByOrderByExperienceDesc(Pageable pageable);
+  
+  @Query("SELECT h FROM history h WHERE h.user.nickname LIKE %:keyword% ORDER BY h.experience DESC")
+  Page<History> searchHistoryByKeyword(String keyword, Pageable pageable);
 }
