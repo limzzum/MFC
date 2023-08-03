7302cae22e79433d0f8626537fb1d62d0f64df6f
euijun <euijun98@naver.com>
2023-08-03 23:27:32 +0900

    feat : 아이템페이지 컴포넌트화

---
+++ frontend/src/components/itempage/item.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeartCirclePlus, faUserClock, faVolumeXmark, faHand, faCross, faSprayCan, faCoins } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
+import styles from "./itemStore.module.css";
+import axios from "axios";
+
+function Item({ icon, name, price, userCoin }) {
+  const [purchasing, setPurchasing] = useState(false);
+
+  const handlePurchase = () => {
+    if (userCoin >= price) {
+      setPurchasing(true);
+      axios
+        .post(`api/item/purchase/{userId}/itemname=${name}`)
+        .then(response => {
+          console.log(`Item ${name} purchased successfully!`);
+        })
+        .catch(error => {
+          console.error("Error purchasing item:", error);
+        })
+        .finally(() => {
+          setPurchasing(false);
+        });
+    } else {
+      alert("잔액이 부족합니다.");
+    }
+  };
+
+  return (
+    <div className={`${styles.itemBox} mx-auto`}>
+      <FontAwesomeIcon icon={icon} size="3x" />
+      
+      <div className="pt-4">{name}</div>
+      <div className="my-1">
+        <FontAwesomeIcon icon={faCoins} color="orange" />
+        {price}
+      </div>
+      <Button
+        className="btn btn-primary"
+        disabled={purchasing}
+        onClick={handlePurchase}
+      >
+        {purchasing ? "구매 중..." : "구입하기"}
+      </Button>
+    </div>
+  );
+}
+
+export default Item;
