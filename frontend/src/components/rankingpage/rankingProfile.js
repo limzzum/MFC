532b037d4df7f83daa5ab01dfb20fccab4cb01ac
euijun <euijun98@naver.com>
2023-08-03 21:43:43 +0900

    feat : 랭킹페이지 css 수정

--- frontend/src/components/rankingpage/rankingProfile.js
+++ frontend/src/components/rankingpage/rankingProfile.js
@@ -1,33 +1,27 @@
 import { useState, useEffect } from "react";
 import styles from './rankingProfile.module.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "react-bootstrap";
 import profileImage from '../../images/img.jpg'
 
 function RankingProfile() {
 
     return (
-        <div className="mt-2">
-          <Container>
-            <Row>
-                <Col className={styles.ranking}>
-                    1
-                </Col>
-            <Col>
-            <div className={`${styles.profileImage} mb-3`}>
-                <img className={`${styles.radiusImg} ${styles.nowImg}`} src={profileImage} alt="profileImage" />
+        <div className={styles.profileBox}>
+            <div className={styles.contentBox}>
+                1
             </div>
-            </Col>
-            <Col className={`${styles.ranking} ${styles.rankNick}`}>테스트</Col>
-            <Col className={styles.ranking}>
-                <p>150pt</p>
-            </Col>
-            <Col className={styles.ranking}>
-                <p>87%</p>
-            </Col>
-            </Row>
-          </Container>
-        </div>
+            
+            <div className={styles.contentBox}>
+                <img className={`${styles.radiusImg} w-50`} src={profileImage} alt="profileImage" />
+            </div>
+            <div className={styles.contentBox}>테스트</div>
+            <div className={styles.contentBox}>
+                150pt
+            </div>
+            <div className={styles.contentBox}>
+                87%
+            </div>
+          </div>
     );
 }
 
