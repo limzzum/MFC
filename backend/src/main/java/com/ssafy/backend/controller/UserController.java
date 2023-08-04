e9478f8cbb06f39f1340da0a5995fb8e6df32f27
임정현 <chfhddl1234@naver.com>
2023-08-04 17:09:37 +0900

    feat : 유저 정보 수정, 탈퇴 API 추가

--- backend/src/main/java/com/ssafy/backend/controller/UserController.java
+++ backend/src/main/java/com/ssafy/backend/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.controller;
 
 import com.ssafy.backend.dto.*;
+import com.ssafy.backend.dto.request.*;
 import com.ssafy.backend.dto.response.*;
 import com.ssafy.backend.entity.*;
 import com.ssafy.backend.security.*;
@@ -8,7 +9,6 @@ import com.ssafy.backend.service.*;
 import java.util.*;
 import javax.validation.*;
 
-import com.ssafy.backend.dto.request.UserRegistDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.*;
 import org.springframework.http.*;
@@ -27,56 +27,59 @@ public class UserController {
     private final EmailService emailService;
 
     @PostMapping("/login")
-    public Message login(@RequestBody @Valid LoginForm loginForm, BindingResult result){
-        if(result.hasErrors()){
+    public Message login(@RequestBody @Valid LoginForm loginForm, BindingResult result) {
+        if (result.hasErrors()) {
             return new Message(HttpStatus.BAD_REQUEST, "로그인 형식이 올바르지 않습니다.", null);
         }
 
         Long loginId = userService.login(loginForm);
-        if(loginId == null){
+        if (loginId == null) {
             return new Message(HttpStatus.OK, "일치하는 사용자가 없습니다.", null);
         }
         String jwtToken = securityService.createJwtToken(String.valueOf(loginId));
-        return new Message(HttpStatus.OK, "로그인 성공", new LoginResultDto(loginId,jwtToken));
+        return new Message(HttpStatus.OK, "로그인 성공", new LoginResultDto(loginId, jwtToken));
     }
 
     @GetMapping("/logout")
-    public ResponseEntity<Message> logout(@RequestHeader("Authorization") String token){
+    public ResponseEntity<Message> logout(@RequestHeader("Authorization") String token) {
         Long userId = Long.valueOf(securityService.getSubject(token));
-        securityService.logout(userId,token);
+        securityService.logout(userId, token);
         return ResponseEntity.ok(new Message(HttpStatus.OK, "로그아웃 성공", null));
     }
 
     @PostMapping
-    public ResponseEntity<Message> regist(@RequestBody @Valid UserRegistDto user, BindingResult result){
-        if(result.hasErrors()){
-            return ResponseEntity.ok(new Message( HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다.", null));
+    public ResponseEntity<Message> regist(@RequestBody @Valid UserRegistDto user,
+        BindingResult result) {
+        if (result.hasErrors()) {
+            return ResponseEntity.ok(new Message(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다.", null));
         }
         String email = user.getEmail();
         String token = UUID.randomUUID().toString();
-        emailService.saveToken(email, token,user);
+        emailService.saveToken(email, token, user);
 
         String message = "Please click the following link to verify your email: " +
-                "<a href='http://i9a605.p.ssafy.io:8081/api/user/verify?email="+email+"&token=" + token + "'>Verify</a>";
+            "<a href='http://i9a605.p.ssafy.io:8081/api/user/verify?email=" + email + "&token="
+            + token + "'>Verify</a>";
         emailService.sendMail(user.getEmail(), "Please verify your email", message);
-        return ResponseEntity.ok(new Message(HttpStatus.ACCEPTED, "이메일 인증번호를 발송하였습니다.",null));
+        return ResponseEntity.ok(new Message(HttpStatus.ACCEPTED, "이메일 인증번호를 발송하였습니다.", null));
 
     }
 
     @GetMapping("/verify")
-    public ResponseEntity<Message> email_verify(@RequestParam String email, @RequestParam String token) {
+    public ResponseEntity<Message> email_verify(@RequestParam String email,
+        @RequestParam String token) {
         String emailToken = emailService.getEmailToken(email);
-        if(emailToken.equals(token)){
+        if (emailToken.equals(token)) {
             UserRegistDto user = emailService.getRegistUserInfo(emailToken);
             Long savedId = userService.regist(user);
-            return ResponseEntity.ok( new Message( HttpStatus.OK, "회원가입 성공", savedId));
+            return ResponseEntity.ok(new Message(HttpStatus.OK, "회원가입 성공", savedId));
         }
-        return ResponseEntity.ok( new Message( HttpStatus.BAD_REQUEST, "회원가입 실패", null));
+        return ResponseEntity.ok(new Message(HttpStatus.BAD_REQUEST, "회원가입 실패", null));
 
     }
 
     @GetMapping
-    public ResponseEntity<Message> info(@RequestHeader("Authorization") String token){
+    public ResponseEntity<Message> info(@RequestHeader("Authorization") String token) {
         System.out.println(securityService.getSubject(token));
         Long userId = Long.valueOf(securityService.getSubject(token));
         User user = userService.findUser(userId);
@@ -84,8 +87,16 @@ public class UserController {
         return ResponseEntity.ok(new Message(HttpStatus.OK, "success", user));
     }
 
+    @PatchMapping
+    public ResponseEntity<Message> modify(@RequestHeader("Authorization") String token,
+                                          @RequestBody UserUpdateDto userUpdateDto) {
+        Long userId = Long.valueOf(securityService.getSubject(token));
+        userService.modify(userId, userUpdateDto);
+        return ResponseEntity.ok(new Message(HttpStatus.OK, "수정 완료", null));
+    }
+
     @DeleteMapping
-    public ResponseEntity<Message> delete(@RequestHeader("Authorization") String token){
+    public ResponseEntity<Message> delete(@RequestHeader("Authorization") String token) {
         Long userId = Long.valueOf(securityService.getSubject(token));
         User user = userService.findUser(userId);
 
@@ -94,9 +105,9 @@ public class UserController {
 
     @GetMapping("/email")
     public ResponseEntity<Message> email_check(@RequestParam String email) {
-        if(userService.isUsedEmail(email)){
-            return ResponseEntity.ok(new Message(HttpStatus.BAD_REQUEST, "이미 사용중인 이메일입니다.",null));
+        if (userService.isUsedEmail(email)) {
+            return ResponseEntity.ok(new Message(HttpStatus.BAD_REQUEST, "이미 사용중인 이메일입니다.", null));
         }
-       return ResponseEntity.ok(new Message(HttpStatus.ACCEPTED, "사용가능한 이메일 입니다.",null));
+        return ResponseEntity.ok(new Message(HttpStatus.ACCEPTED, "사용가능한 이메일 입니다.", null));
     }
 }
