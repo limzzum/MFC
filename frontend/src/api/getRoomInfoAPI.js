520f7e871f5104a7148f34edbc66136b42be4430
김상연 <ksang98@icloud.com>
2023-08-04 16:17:51 +0900

    feat: 토론방 정보 가져오기 API 호출

---
+++ frontend/src/api/getRoomInfoAPI.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://i9a605.p.ssafy.io:8081/api/debate'
+
+export const getRoomInfo = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/1`);  // 추후에 클릭한 토론방 번호 들어가도록 수정 필요
+        return response.data;
+    } catch (e) {
+        console.log(`토론방 정보 API 오류: ${e}`);
+        return null;
+    }
+};
