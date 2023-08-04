621268e39bf868e21453d4af21e6540c2b9568fb
김상연 <ksang98@icloud.com>
2023-08-04 09:00:03 +0900

    feat: NavBar 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import NavBar from './components/navBar/navBar';
 import LoginPage from './pages/login/loginPage';
 import PasswordChangePage from './pages/passwordchange/passwordChangePage';
@@ -11,54 +11,35 @@ import MyProfilePage from './pages/myprofile/myProfile';
 import RankingPage from './pages/ranking/ranking';
 import ItemPage from './pages/item/itemPage';
 
+function NavBarWrapper() {
+  const location = useLocation();
+  
+  const hideNavBar = location.pathname === '/debateRoom';
+  
+  return (
+    <>
+      { !hideNavBar && <NavBar />}
+    </>
+  );
+}
+
 function App() {
+
+
   return (
     <BrowserRouter>
       <div className='App'>
-        {/* <nav>
-          <ul className='horizontal-align'>
-            <li>
-              <Link to='/pages/login/loginPage'>loginPage</Link>
-            </li>
-            <li>
-              <Link to='/pages/passwordchange/passwordChangePage'>passwordChange</Link>
-            </li>
-            <li>
-              <Link to='/pages/debateRoom/debatePage.jsx'>debatePage</Link>
-            </li>
-            <li>
-              <Link to='/pages/signup/signupPage'>signupPage</Link>
-            </li>
-            <li>
-              <Link to='/pages/myprofile/myProfile/:userId'>myProfilePage</Link>
-            </li>
-            <li>
-              <Link to='/pages/ranking/ranking'>rankingPage</Link>
-            </li>
-            <li>
-              <Link to='/pages/main/mainPage'>mainPage</Link>
-            </li>
-            <li>
-              <Link to='/pages/item/itempage/:userId'>itemPage</Link>
-            </li>
-          </ul>
-        </nav> */}
-        <NavBar />
+        <NavBarWrapper />
         <Routes>
           <Route path='/login' element={<LoginPage />} />
           <Route path='/pwChange/:userId' element={<PasswordChangePage />} />
           <Route path='/signup' element={<SignupPage />} />
           <Route path='/' element={<MainPage />} />
-          <Route path='/debateRoom' element={<DebatePage/>}>
-          </Route>  
-          <Route path='/signup' element={<SignupPage/>}>
-          </Route>
-          <Route path='/profile' element={<MyProfilePage/>}>
-          </Route>
-          <Route path='/ranking' element={<RankingPage/>}>
-          </Route>
-          <Route path='/item' element={<ItemPage/>}>
-          </Route>
+          <Route path='/debateRoom' element={<DebatePage/>} />
+          <Route path='/signup' element={<SignupPage/>} />
+          <Route path='/profile' element={<MyProfilePage/>} />
+          <Route path='/ranking' element={<RankingPage/>} />
+          <Route path='/item' element={<ItemPage/>} />
         </Routes>
       </div>
     </BrowserRouter>
