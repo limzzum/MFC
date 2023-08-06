67f1fc86eca681040944ed79432d637b8488a5c2
김상연 <ksang98@icloud.com>
2023-08-06 23:45:18 +0900

    feat: 투표 현황 API 호출 후 데이터 전역 저장

--- frontend/src/recoil/debateStateAtom.js
+++ frontend/src/recoil/debateStateAtom.js
@@ -1,10 +1,16 @@
 import {atom, selector, useRecoilState} from 'recoil';
 import {getRoomInfo} from '../api/getRoomInfoAPI';
+import {getVoteResult} from '../api/getVoteResultAPI'
 
 export const debateRoomState = atom({
     key: 'debateRoomState',
     default: {},
 });
+
+export const voteResultState = atom({
+    key: 'voteResultState',
+    default: {},
+})
 
 // API 호출 결과를 Recoil selector로 관리
 export const getDebateRoomState = selector({
@@ -21,6 +27,21 @@ export const getDebateRoomState = selector({
         
     },
 });
+
+export const getVoteResultState = selector({
+    key: 'getVoteResultState',
+    get: async () => {
+        const data = await getVoteResult();
+        
+        if(data) {
+            // API 호출 결과가 있으면, voteResultState에 저장
+            return data;
+        } else{
+            return null;
+        }
+
+    }
+})
 
 
 export const statusState = atom({
