f98fa99c105e111cfe134bddf7b7df3a380cdce4
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-08-07 15:51:04 +0900

    feat : 채팅메시지 소켓 연결 구현

---
+++ backend/src/main/java/com/ssafy/backend/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.ssafy.backend.controller;
+
+import com.ssafy.backend.dto.common.ChatMessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequiredArgsConstructor
+public class ChatController {
+
+  private final SimpMessagingTemplate messagingTemplate;
+
+  @MessageMapping("/chat")
+  public void sendMessage(ChatMessageDto chatMessageDto) {
+    Long roomId = chatMessageDto.getRoomId();
+    messagingTemplate.convertAndSend("/from/chat/" + roomId, chatMessageDto);
+  }
+
+}
