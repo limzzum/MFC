09fd132e71b32da576110e6ce293f7587a10e8b8
kosigi19 <kosigi19@naver.com>
2023-08-07 12:57:47 +0900

    feat : 토론방 나가기 API 개발 완료

--- backend/src/main/java/com/ssafy/backend/repository/PlayerRepository.java
+++ backend/src/main/java/com/ssafy/backend/repository/PlayerRepository.java
@@ -1,17 +1,23 @@
 package com.ssafy.backend.repository;
 
 import com.ssafy.backend.entity.Player;
-
 import java.util.List;
 import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
-  Optional<Player> findFirstByRoomIdAndIsTopicTypeA(Long roomId,Boolean isTopicTypeA);
+  Optional<Player> findFirstByRoomIdAndIsTopicTypeA(Long roomId, Boolean isTopicTypeA);
 
   List<Player> findAllByRoomId(Long roomId);
 
   Optional<Player> findTopByRoomIdAndUserId(Long roomId, Long userId);
 
+  @Modifying
+  @Transactional
+  @Query("UPDATE Player SET user.id = null WHERE room.id = :roomId and user.id = :userId")
+  void resetUserId(Long roomId, Long userId);
 }
