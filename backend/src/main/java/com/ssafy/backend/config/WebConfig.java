1792c9125eb81d7add2b1c7dc8fe871d0b9d708f
jiwon <wldnjs9515@naver.com>
2023-08-08 23:45:10 +0900

    feat : WebConfig cros 설정

--- backend/src/main/java/com/ssafy/backend/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/WebConfig.java
@@ -5,11 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://goldenteam.site","http://goldenteam.site")// 허용할 출처
-                .allowCredentials(true); // 쿠키 인증 요청 허용
-    }
+public class WebConfig {
+//     implements WebMvcConfigurer
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000", "https://goldenteam.site","http://goldenteam.site");// 허용할 출처
+////                .allowCredentials(true); // 쿠키 인증 요청 허용
+//    }
 }
