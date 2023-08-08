563bd6a7ad04357036bdb052876104ee3bc6b561
김상연 <ksang98@icloud.com>
2023-08-08 10:28:39 +0900

    feat: API 호출 시 방 번호 추가

--- frontend/src/api/getVoteResultAPI.js
+++ frontend/src/api/getVoteResultAPI.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const BASE_URL = 'http://i9a605.p.ssafy.io:8081/api/viewer/vote'
 
-export const getVoteResult = async () => {
+export const getVoteResult = async (roomId) => {
     try {
-        const response = await axios.get(`${BASE_URL}/1`);  // 추후에 클릭한 토론방 번호 들어가도록 수정 필요
+        const response = await axios.get(`${BASE_URL}/${roomId}`);  // 추후에 클릭한 토론방 번호 들어가도록 수정 필요
         return response.data;
     } catch (e) {
         console.log(`토론방 정보 API 오류: ${e}`);
