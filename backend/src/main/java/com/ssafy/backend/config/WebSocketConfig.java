9d06ffa9d3fe877ba6ad5f5839e2954f8a14dc34
jiwon <wldnjs9515@naver.com>
2023-08-08 23:20:03 +0900

    feat : websocket config Cros오류수정

--- backend/src/main/java/com/ssafy/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/WebSocketConfig.java
@@ -17,6 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/mfc").setAllowedOriginPatterns("*").withSockJS();
+    registry.addEndpoint("/mfc").setAllowedOrigins("http://localhost:3000", "https://goldenteam.site","http://goldenteam.site").withSockJS();
+
   }
 }
