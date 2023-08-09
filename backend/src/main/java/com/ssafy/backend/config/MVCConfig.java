7291034f5de3df815a36439753e1ef944bb6edc3
DESKTOP-1JVI7QI\SSAFY <wldnjs9515@naver.com>
2023-08-09 10:36:33 +0900

    feat : Swwagger주소삭제

--- backend/src/main/java/com/ssafy/backend/config/MVCConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/MVCConfig.java
@@ -17,21 +17,25 @@ public class MVCConfig implements WebMvcConfigurer {
 //                        "/**/*.css","/**/*.jpg","/**/*.jpeg","/**/*.png","/**/*.peg", "/**/*.js", "/error/**"
 //                    );
 //    }
-//@Override
-//public void addCorsMappings(CorsRegistry registry) {
-//    registry.addMapping("/**")
-//        .allowedOrigins("*")
-//        .allowedMethods("*");
-//}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "https://goldenteam.site") // 허용할 출처
+            .allowedMethods("GET", "POST","PATCH","DELETE","PUT") // 허용할 HTTP method'
+            .allowedHeaders("content-type")
+            .allowedHeaders("Authorization")
+            .allowCredentials(true) // 쿠키 인증 요청 허용
+            .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
 
     @Bean
     public LoginCheckInterCeptor loginCheckInterCeptor(){
         return new LoginCheckInterCeptor();
     }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("redirect:/api/swagger-ui/index.html");
-    }
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("redirect:/api/swagger-ui/index.html");
+//    }
 
 }
