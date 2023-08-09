b7cfe387d6aad059f1d4e65e77acb6b498327d65
kosigi19 <kosigi19@naver.com>
2023-08-09 13:45:30 +0900

    feat:CORS 예외처리

--- backend/src/main/java/com/ssafy/backend/config/MVCConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/MVCConfig.java
@@ -12,15 +12,15 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 @EnableWebSocketMessageBroker
 public class MVCConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginCheckInterCeptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/", "/api/user", "/api/user/login", "/api/user/logout/**","/api/user/refresh/**","/api/place/**",
-                        "/**/*.css","/**/*.jpg","/**/*.jpeg","/**/*.png","/**/*.peg", "/**/*.js", "/error/**"
-                    );
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginCheckInterCeptor())
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/", "/api/user", "/api/user/login", "/api/user/logout/**","/api/user/refresh/**","/api/place/**",
+//                        "/**/*.css","/**/*.jpg","/**/*.jpeg","/**/*.png","/**/*.peg", "/**/*.js", "/error/**"
+//                    );
+//    }
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**")
