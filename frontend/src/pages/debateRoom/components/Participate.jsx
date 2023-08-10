f390e7f16be42da07bbf11e25e277d12f13420e8
suchshin <scshin97@gmail.com>
2023-08-10 10:47:16 +0900

    서버 빌드를 위한 수정 사항

--- frontend/src/pages/debateRoom/components/Participate.jsx
+++ frontend/src/pages/debateRoom/components/Participate.jsx
@@ -1,8 +1,23 @@
-import React from "react";
-import {  Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios"; 
 import style from '../debatePage.module.css';
 
+import { useRecoilValue } from 'recoil';
+import { userInfoState } from '../../../recoil/userInfo';
+
 function Participate({status, role, onRoleChange, playerStatus, setPlayerStatus}){
+    const userInfo = useRecoilValue(userInfoState); // 리코일 상태에서 유저 정보 가져오기
+    const [, setUserProfileImage] = useState(null); // 유저 프로필 이미지 상태
+
+    const handleParticipate = async () => {
+        const response = await axios.get(`Yhttps://goldenteam.site/api/user/${userInfo.id}`);
+        if (response.data && response.data.imageURL) {
+            setUserProfileImage(response.data.imageURL);
+        }
+        // 여기에 기존에 있던 onRoleChange, setPlayerStatus 로직 추가
+    };
+
     return (
         <div>
             <Row>
@@ -14,6 +29,7 @@ function Participate({status, role, onRoleChange, playerStatus, setPlayerStatus}
                             <button 
                                 className={style.button} 
                                 onClick={() => {
+                                    handleParticipate();
                                     onRoleChange('participant');
                                     setPlayerStatus((prevStatus) => [true, prevStatus[1]]);
                                 }}
@@ -32,6 +48,7 @@ function Participate({status, role, onRoleChange, playerStatus, setPlayerStatus}
                             <button 
                                 className={style.button} 
                                 onClick={() => {
+                                    handleParticipate(); // 추가: 참여하기 버튼 클릭시 handleParticipate 함수 호출
                                     onRoleChange('participant');
                                     setPlayerStatus((prevStatus) => [prevStatus[0], true]);
                                 }}
