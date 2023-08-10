c69a017c3b9237cd2ee791b0fb9bf2da854e4ab0
limzzum <chfhddl1234@naver.com>
2023-08-11 08:47:47 +0900

    feat : player 등록, 준비 상태 변경 결과 dto

---
+++ backend/src/main/java/com/ssafy/backend/dto/socket/response/PlayerInfoDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.backend.dto.socket.response;
+
+import com.ssafy.backend.entity.ItemCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PlayerInfoDto {
+
+    private String nickname;
+    private String profile;
+    private ItemCode colorItem;
+    private boolean isTopicA;
+    private boolean isReady;
+}
