d75908cb8fd5befe4f67e706afefa7f87e68e337
suchshin <scshin97@gmail.com>
2023-08-10 15:05:32 +0900

    feat : 토론카드에 프로필 사진 넣음

--- frontend/src/components/mainpage/debateRoomCard.js
+++ frontend/src/components/mainpage/debateRoomCard.js
@@ -1,18 +1,18 @@
 import React from "react";
 import axios from "axios";
 import { useRecoilValue } from "recoil";
-import { userIdState } from "../../recoil/userId";
+import { userIdState } from "../../recoil/userId"; 
+import { userInfoState } from "../../recoil/userInfo"; 
 import { BsStopwatch } from "react-icons/bs";
 import { RiSpeakLine } from "react-icons/ri";
 import personImage from "../../images/person.png";
 import style from "./debateRoomCard.module.css";
 import { useNavigate } from "react-router-dom";
 
-const DebateRoomCard = ({ title1, title2, debateTime, speechTime, roomId }) => {
+const DebateRoomCard = ({ title1, title2, debateTime, speechTime, roomId, userProfileImg1, userProfileImg2 }) => {
   const userId = useRecoilValue(userIdState);
+  const userInfo = useRecoilValue(userInfoState);
   const navigate = useNavigate();
-
-  // console.log(userId);
 
   const handleJoinClick = async () => {
     const url = `https://goldenteam.site/api/viewer/${roomId}/${userId.userId}`;
@@ -20,24 +20,23 @@ const DebateRoomCard = ({ title1, title2, debateTime, speechTime, roomId }) => {
       roomid: roomId,
       userid: userId.userId,
     });
-
+    console.log(userInfo)
     if (response.status === 200) {
       console.log("이동 성공");
       console.log(response);
-      // 여기까지는 성공
-
       navigate(`/debateRoom/${roomId}`);
     }
   };
+
   return (
     <div className="">
       <div className="card" style={{ width: "18rem" }}>
         <div className="d-flex">
           <div className={style.imgbox}>
-            <img src={personImage} style={{ maxWidth: "100%" }} alt="none" />
+              <img className={style.fullWidthImage} src={userProfileImg1 ? `https://goldenteam.site/profiles/${userProfileImg1}` : personImage} alt="User1" />
           </div>
           <div className={style.imgbox}>
-            <img src={personImage} style={{ maxWidth: "100%" }} alt="none" />
+              <img className={style.fullWidthImage} src={userProfileImg2 ? `https://goldenteam.site/profiles/${userProfileImg2}` : personImage} alt="User2" />
           </div>
         </div>
         <div className={style.cardbody}>
