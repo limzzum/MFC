d14f04e82871612ca02fd28d0fbbd7bad1cb11de
임정현 <chfhddl1234@naver.com>
2023-08-11 17:40:17 +0900

    feat : 플레이어 삭제 소켓 api 추가

--- backend/src/main/java/com/ssafy/backend/controller/PlayerSocketController.java
+++ backend/src/main/java/com/ssafy/backend/controller/PlayerSocketController.java
@@ -27,9 +27,19 @@ public class PlayerSocketController {
     public void setPlayer(PlayerRequestDto playerDto) {
         Long roomId = playerDto.getRoomId();
         User user = userService.findById(playerDto.getUserId());
-        playerService.regist(PlayerRegistDto.builder().roomId(roomId).userId(user.getId()).isATopic(playerDto.isTopicA()).build());
+        playerService.regist(PlayerRegistDto.builder().roomId(roomId).userId(user.getId()).isATopic(playerDto.isATopic()).build());
         PlayerInfoDto playerInfoDto = PlayerInfoDto.builder().userId(user.getId()).nickname(user.getNickname()).profile(user.getProfile())
-                        .colorItem(user.getColorItem()).isReady(false).isTopicA(playerDto.isTopicA()).build();
+                        .colorItem(user.getColorItem()).isReady(false).isTopicA(playerDto.isATopic()).build();
+        messagingTemplate.convertAndSend("/from/player/" + roomId, playerInfoDto);
+    }
+
+    @MessageMapping("/player/out")
+    public void playerOut(PlayerRequestDto playerDto) {
+        Long roomId = playerDto.getRoomId();
+        User user = userService.findById(playerDto.getUserId());
+        playerService.deletePlayer(PlayerRegistDto.builder().roomId(roomId).userId(user.getId()).isATopic(playerDto.isATopic()).build());
+        PlayerInfoDto playerInfoDto = PlayerInfoDto.builder().userId(null).nickname(null).profile(null)
+            .colorItem(null).isReady(false).isTopicA(false).build();
         messagingTemplate.convertAndSend("/from/player/" + roomId, playerInfoDto);
     }
 
@@ -39,9 +49,11 @@ public class PlayerSocketController {
         User user = userService.findById(playerDto.getUserId());
         playerService.changeStatus(new PlayerDto(roomId,user.getId()), playerDto.isReady());
         PlayerInfoDto playerInfoDto = PlayerInfoDto.builder().userId(user.getId()).nickname(user.getNickname()).profile(user.getProfile())
-                .colorItem(user.getColorItem()).isReady(playerDto.isReady()).isTopicA(playerDto.isTopicA()).build();
+                .colorItem(user.getColorItem()).isReady(playerDto.isReady()).isTopicA(playerDto.isATopic()).build();
         messagingTemplate.convertAndSend("/from/player/" + roomId, playerInfoDto);
     }
+
+
 
     @MessageMapping("/player/item")
     public void useItem(PlayerItemDto playerDto) {
@@ -51,11 +63,11 @@ public class PlayerSocketController {
         User user = userService.findById(playerDto.getUserId());
         if(usedItem.equals("아이템 사용 가능")){
             PlayerItemInfoDto playerItemInfoDto = PlayerItemInfoDto.builder().userId(user.getId()).nickname(user.getNickname())
-                            .isTopicA(playerDto.isTopicA()).itemCodeId(playerDto.getItemCodeId()).build();
+                .isTopicA(playerDto.isTopicA()).itemCodeId(playerDto.getItemCodeId()).build();
             messagingTemplate.convertAndSend("/from/player/" + roomId,playerItemInfoDto );
         }else {
             ItemFailDto itemFailDto = ItemFailDto.builder().userId(user.getId()).nickname(user.getNickname()).itemCodeId(playerDto
-                    .getItemCodeId()).message("아이템 사용 불가능").build();
+                .getItemCodeId()).message("아이템 사용 불가능").build();
             messagingTemplate.convertAndSend("/from/player/" + roomId,itemFailDto );
         }
     }
