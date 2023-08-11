ad0ba668581720e9adc5bb4c2d27994f33c2c58f
limzzum <chfhddl1234@naver.com>
2023-08-11 09:23:12 +0900

    feat : @Builder 추가

--- backend/src/main/java/com/ssafy/backend/dto/request/PlayerRegistDto.java
+++ backend/src/main/java/com/ssafy/backend/dto/request/PlayerRegistDto.java
@@ -2,12 +2,14 @@ package com.ssafy.backend.dto.request;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class PlayerRegistDto {
     private Long roomId;
     private Long userId;
