62eed0d8c19ad96f9b3478fc6d1d055481be9e79
euijun <euijun98@naver.com>
2023-08-13 02:20:10 +0900

    feat : 이미지 공유 1차 아직 미완

--- frontend/src/pages/debateRoom/components/ScreenShare.jsx
+++ frontend/src/pages/debateRoom/components/ScreenShare.jsx
@@ -1,15 +1,41 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Button } from 'react-bootstrap';
 import style from '../debatePage.module.css';
+import { Button } from 'react-bootstrap';
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client"; 
 import axios from 'axios'
 
-function ScreenShare({ status, role }) {
+function ScreenShare({ roomId, role }) {
     const [selectedImage, setSelectedImage] = useState(null);
+    const [imgFileName, setImgFileName] = useState("")  
     const imageInputRef = useRef();
+    const stompRef = useRef(null);
+
+    useEffect(() => {
+        var sock = new SockJS("http://localhost:8081/mfc");
+        var stomp = Stomp.over(sock);
+        stomp.connect({}, function () {
+    // 이 부분 조금 수상 재참조하고, 구독하는 부분
+        stompRef.current = stomp;  
+        stomp.subscribe(`/from/room/file/${roomId}`, (message) => {
+        console.log(JSON.parse(message.body));
+    // 이전 메시들에 새로운 메시지를 추가해서 chatMessages를 업데이트
+        setImgFileName(message.data);
+          });
+        });
+        
+        return () => {
+          if (stompRef.current) {
+            stompRef.current.disconnect();
+          }
+        };
+      }, [roomId, imgFileName]);
 
     const handleImageChange = async(event) => {
         const file = event.target.files[0];
         setSelectedImage(file);
+        
+        // 이미지 등록 서버 저장
         const config = {
             headers: {
               "Content-Type": "multipart/form-data",
@@ -25,9 +51,11 @@ function ScreenShare({ status, role }) {
                 formData,
                 config
                 );
-        
                 console.log("이미지 업데이트 응답:", response.data);
-                // 이후 필요한 작업 수행
+                // const imageFileName  = response.data.data
+                const stompMessage = JSON.stringify({ img: file })
+                console.log(stompMessage)
+                stompRef.current.send("/to/room/file/{roomId}", {}, stompMessage);
             } catch (error) {
                 console.error("이미지 업데이트 오류:", error);
             }
@@ -46,8 +74,7 @@ function ScreenShare({ status, role }) {
                             src={URL.createObjectURL(selectedImage)}
                             alt="Uploaded"
                             className={style.uploadedImage}
-                                style={{ objectFit: "contain" }}
-
+                            style={{ objectFit: "contain" }}
                         />
                         <Button variant="danger" onClick={handleRemoveImage}>
                             이미지 제거
