d354bd5dd691d21de7f91f23b70528732fc3bbfd
limzzum <chfhddl1234@naver.com>
2023-08-13 23:18:13 +0900

    feat : RoomStatusDto 추가

---
+++ backend/src/main/java/com/ssafy/backend/dto/socket/response/RoomStatusDto.java
@@ -0,0 +1,22 @@
+package com.ssafy.backend.dto.socket.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoomStatusDto {
+    private LocalDateTime DebateStartTime;
+    private Long curUserId;
+    private boolean isATurn;
+    private LocalDateTime startTalkTime;
+    private int hpPointA;
+    private int hpPointB;
+    private String roomImagePath;
+}
