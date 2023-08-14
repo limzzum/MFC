c3e8fde69889fd9631e8dad09640b747be5fb1d5
limzzum <chfhddl1234@naver.com>
2023-08-14 14:04:45 +0900

    feat : isATopic 필드 추가

--- backend/src/main/java/com/ssafy/backend/dto/request/PenaltyRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/dto/request/PenaltyRequestDto.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class PenaltyRequestDto {
     private Long roomId;
     private Long userId;
+    private boolean isATopic;
     private Long penaltyCodeId;
 
 }
