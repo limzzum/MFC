7c8ed6d852a5a3e48af796af1a25c812ec2024d8
김의준 <euijun98@naver.com>
2023-08-16 17:50:10 +0900

    feat : 비속어 집가서 isATopic만 바꾸기

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-// export const SOCKET_BASE_URL = "http://localhost:8081/mfc";
-export const SOCKET_BASE_URL = "https://goldenteam.site/mfc";
+export const SOCKET_BASE_URL = "http://localhost:8081/mfc";
+// export const SOCKET_BASE_URL = "https://goldenteam.site/mfc";
 export const AXIOS_BASE_URL = "https://goldenteam.site/api";
