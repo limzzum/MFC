28a559103ac9cefd590eb516bc9d44541cfca11f
seokchul <scshin97@gmail.com>
2023-08-16 17:35:41 +0900

    관전자로 내려가기 수정 및 머지

--- frontend/src/pages/debateRoom/components/Participate.jsx
+++ frontend/src/pages/debateRoom/components/Participate.jsx
@@ -1,4 +1,4 @@
-import  { useRef } from "react";
+import { useRef } from "react";
 import { Row, Col } from "react-bootstrap";
 import style from "../debatePage.module.css";
 import UserVideoComponent from "../Openvidu/UserVideoComponent";
@@ -38,19 +38,6 @@ function Participate({
     }
   };
 
-  const [showImageSegmentation, setShowImageSegmentation] = useState(false);
-  const handleParticipantClick = (isTopicA) => {
-    onRoleChange("participant");
-    if (isTopicA) {
-      setPlayerStatus((prevStatus) => [true, prevStatus[1]]);
-      handlePlayerAVideoStream(publisher);
-    } else {
-      setPlayerStatus((prevStatus) => [prevStatus[0], true]);
-      handlePlayerBVideoStream(publisher);
-    }
-    handlePostPlayer(isTopicA);
-    setShowImageSegmentation(true); // 참여하기 버튼을 클릭하면 이미지 세그멘테이션 기능 활성화
-  };
   return (
     <div>
       <Row className={`m-0 p-0`}>
@@ -81,28 +68,35 @@ function Participate({
           {/* <div className={`${style.rightCount} m-0 p-0 mx-auto`}>
             <span>남은 연장 횟수: </span>
           </div> */}
+          <AudioSegmentationComponent roomId={roomId} userId={userId} />
         </Col>
         <Col xs={1} className={`m-0 p-0`}></Col>
         <Col className={`m-0 p-0`}>
-          <div className={`${style.Participant} mx-auto`} style={{ position: 'relative' }}>
-            <div style={{ position: 'relative', width: '100%', paddingBottom: '75%' }}>
-              { playerB === undefined && playerStatus[1] === false && status === "waiting" && (
-                <button
-                  className={`${style.button} btn`}
-                  onClick={() => handleParticipantClick(false)}
-                >
-                  참가하기
-                </button>
-              )}
-              { playerB !== undefined && (
-                <UserVideoComponent
-                  className="playerB"
-                  streamManager={playerB}
-                  called={style.Participant}
-                />
-              )}
-            </div>
+          <div className={`${style.Participant} mx-auto`}>
+            { playerB === undefined && playerStatus[1] === false && status === "waiting" && (
+              <button
+                className={`${style.button} btn`}
+                onClick={() => {
+                  onRoleChange("participant");
+                  setPlayerStatus((prevStatus) => [prevStatus[0], true]);
+                  handlePlayerBVideoStream(publisher);
+                  handlePostPlayer(false);
+                }}
+              >
+                참가하기
+              </button>
+            )}
+            { playerB !== undefined && (
+              <UserVideoComponent
+                className="playerB"
+                streamManager={playerB}
+                called={style.Participant}
+              />
+            )}
           </div>
+          {/* <div className={`${style.rightCount} m-0 p-0 mx-auto`}>
+            <span>남은 연장 횟수: </span>
+          </div> */}
         </Col>
       </Row>
     </div>
